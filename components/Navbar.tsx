be43c10c6db23475e0c8008153d36ff0cbd608ed
Karmpatel18 <karmpatel1203@gmail.com>
2025-09-01 15:32:05 +0530

    project init

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { AnimatedThemeToggler } from "@/components/magicui/animated-theme-toggler";
+function Navbar() {
+  return (
+    <nav className="flex w-full max-w-full justify-between items-center sm:px-4 md:px-6 lg:px-12 px-2 py-3.5 border-b-[1px] dark:border-b-neutral-800 border-b-neutral-200">
+        <h1 className="text-2xl font-semibold tracking-tight dark:text-neutral-50 text-neutral-900">LearnML</h1>
+        <div className="flex items-center gap-4">
+          <AnimatedThemeToggler />
+          <button className="flex cursor-pointer text-sm gap-2 items-center bg-neutral-50 dark:bg-neutral-950 dark:text-white border-[1px] border-neutral-200 text-neutral-900 dark:border-neutral-800 px-4 py-2 rounded-lg tracking-tighter"><FaGithub size={18}/> Star Project</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
